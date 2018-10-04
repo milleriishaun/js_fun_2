04fadf19c284d84eae3107129af1d6344f40098d
milleriishaun <milleriishaun@gmail.com>
2018-10-03 20:53:58 -0700

    Add reduce_snippet context

---
+++ README.md
@@ -0,0 +1,7 @@
+<!-- jsfun2 uses examples/algorithms from FreeCodeCamp -->
+<!-- to start, I changed launch.json to include "programs":"${file}" to run current file with f5 -->
+<!-- Note: it will likely need to be changed to ${workspace folder}\\index.js like jsfun if testing within a browser. -->
+<!-- Prettier "fix on save" appears to work, even though I was using react previously. -->
+<!-- yarn add --dev eslint prettier eslint-config-airbnb@^latest eslint-config-prettier eslint-plugin-prettier eslint-plugin-react eslint-plugin-import eslint-plugin-jsx-a11y@^latest -->
+<!-- the above will get eslint to work correctly, except do not include react and jsx -->
+<!-- everything appears to be in working order -->
--- reduce_snippets.js
+++ reduce_snippets.js
@@ -3,42 +3,49 @@
 // Remember that .reduce() can create .map() and .filter(), so it also can be used for any kind of repeated string transformation or array manipulation that would otherwise require a loop.
 
 // Count the number of elements in two different arrays
-var arr = arr1.concat(arr2);
-return arr.reduce(function(prev, next) {
-  prev[next] = prev[next] + 1 || 1;
-  return prev;
-}, {});
+function countDiff(arr1, arr2) {
+  let arr = arr1.concat(arr2);
+  return arr.reduce(function(prev, next) {
+    prev[next] = prev[next] + 1 || 1;
+    return prev;
+  }, {});
+}
+console.log(countDiff([1, 3, 5, 6, 7, 7, 8, 9], [1, 3, 5, 6, 7, 7, 6, 9]));
 
+/*
 // Get rid of Duplicates in an array
 arr.reduce((prev, num) => {
-  if (prev.indexOf(num) === -1) {
+  if(prev.indexOf(num) === -1) {
     prev.push(num);
   }
-  return prev;
-}, []);
+  return prev
+},[]);
+
 
 // Sum up all values within an array
-var total = [0, 1, 2, 3].reduceRight(function(a, b) {
+let total = [0, 1, 2, 3].reduceRight(function(a, b) {
   return a + b;
 });
 
-or;
+or
 
 // Sum up all values within an Argument
-var arr = Array.from(arguments).reduce(function(a, b) {
+let arr = Array.from(arguments).reduce(function(a, b) {
   return a + b;
 });
 
-or;
+or
 
 // Sum up all values with arrow functions
-var sum = arr.reduce((a, b) => a + b);
+let sum = arr.reduce((a, b) => a + b);
 
 // total == 6
 
+
 // Flatten an array of arrays
-var flattened = [[0, 1], [2, 3], [4, 5]].reduceRight(function(a, b) {
-  return a.concat(b);
+let flattened = [[0, 1], [2, 3], [4,5]].reduceRight(function(a, b) {
+    return a.concat(b);
 }, []);
 
 // flattened is [4, 5, 2, 3, 0, 1]
+*/
