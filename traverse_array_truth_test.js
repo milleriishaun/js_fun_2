4a764d8c4910eb2f8f0bf0ee959395fb20b7448b
milleriishaun <milleriishaun@gmail.com>
2018-10-06 21:59:56 -0700

    Changed for to while traverse_array_truth_test.js

--- traverse_array_truth_test.js
+++ traverse_array_truth_test.js
@@ -1,6 +1,18 @@
+/*
+*/
+// my initial solution
+// time: 20 mins.
 function findElement(arr, func) {
   let num = 0;
-  return num;
+  let truthArr = arr.map(func); //trip: idk how to use func arrow
+  let i = 0;
+  while (i < truthArr.length) {
+    if (truthArr[i] === true) {
+      return arr[i];
+    }
+    i++;
+  }
+  return undefined;
 }
-
-findElement([1, 2, 3, 4], num => num % 2 === 0);
+//note: this solution feels very bloated
+console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
