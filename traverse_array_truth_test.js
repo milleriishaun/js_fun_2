efe57d1165640b65c7e696302311f852fde9b87a
milleriishaun <milleriishaun@gmail.com>
2018-10-07 22:32:08 -0700

    Edit example traverse_array_truth_test.js

--- traverse_array_truth_test.js
+++ traverse_array_truth_test.js
@@ -16,3 +16,41 @@ function findElement(arr, func) {
 }
 //note: this solution feels very bloated
 console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
+
+/*
+// This is something interesting about the function arrow:
+var elements = [
+  'Hydrogen',
+  'Helium',
+  'Lithium',
+  'Beryllium'
+];
+
+elements.map(function(element ) {
+  return element.length;
+}); // [8, 6, 7, 9]
+
+elements.map(element => {
+  return element.length;
+}); // [8, 6, 7, 9]
+
+elements.map(element => element.length); // [8, 6, 7, 9]
+
+elements.map(({ length }) => length); // [8, 6, 7, 9], especially interesting.
+*/
+
+/*
+// This example is interesting too, since it describes how 'this' ought to be
+function Person() {
+  this.age = 0;
+
+  setInterval(() => {
+    // remember that setInterval used to refer to the 'this' in
+    // global scope rather than the 'this' in Person object(ES5)
+    // But the usage here refers to ES6 fix.
+    this.age++; // |this| properly refers to the Person object
+  }, 1000);
+}
+
+var p = new Person();
+*/
