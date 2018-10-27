4cdcf603aa2b85bf434814b89fa6f18128aba054
milleriishaun <milleriishaun@gmail.com>
2018-10-27 12:04:32 -0700

    Add IntAlg_missing_letters.js

--- IntAlg_DNA_pairing.js
+++ IntAlg_DNA_pairing.js
@@ -31,6 +31,7 @@ console.log(pairElement('ATCGA')); // [["A","T"],["T","A"],["C","G"],["G","C"],[
 // console.log(pairElement('TTGAG')); // [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]]
 // console.log(pairElement('CTCTA')); // [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]]
 
+/*
 // my initial solution 2
 // time: 40 mins // way too long, but I tired something new
 // I'm a actually just glad it worked
@@ -61,3 +62,55 @@ function pairElement(str) {
 
   return newArr;
 }
+*/
+
+/*
+// Basic Solution
+function pairElement(str) {
+  // Return each strand as an array of two elements, the original and the pair.
+  var paired = [];
+
+  // Function to check with strand to pair.
+  var search = function(char) {
+    switch (char) {
+      case 'A':
+        paired.push(['A', 'T']);
+        break;
+      case 'T':
+        paired.push(['T', 'A']);
+        break;
+      case 'C':
+        paired.push(['C', 'G']);
+        break;
+      case 'G':
+        paired.push(['G', 'C']);
+        break;
+    }
+  };
+
+  // Loops through the input and pair.
+  for (var i = 0; i < str.length; i++) {
+    search(str[i]);
+  }
+
+  return paired;
+} // I like this solution because it is what I wanted my solution to be
+// It was eye-opening to just push correct value arrays into the final array.
+*/
+
+// Intermediate Solution
+function pairElement(str) {
+  //create object for pair lookup
+  var pairs = {
+    A: 'T',
+    T: 'A',
+    C: 'G',
+    G: 'C'
+  };
+  //split string into array of characters
+  var arr = str.split('');
+  //map character to array of character and matching pair
+  return arr.map(x => [x, pairs[x]]);
+} // I like this solution because it looks very tight and clean
+// The key is to make the key,value pairs object... then returning the map
+// of the correct pairs... each in their own 2 index position arrays.
---
+++ IntAlg_missing_letters.js
@@ -0,0 +1,21 @@
+// my inital solution
+// time: 10 mins. //fail: didn't really work, so I had to move it here.
+function fearNotLetter(str) {
+  let arr = str.split();
+  let start = 97;
+  let end = 122;
+  let startx = arr[0];
+  let endx = arr[arr.length];
+  let code = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = startx; j < endx; j++) {
+      if (arr[j] !== String.fromCharCode(j)) {
+        return (code = String.fromCharCode(j));
+      }
+    }
+    return undefined;
+  }
+  return code;
+}
+
+fearNotLetter('abce');
