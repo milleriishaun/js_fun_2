797bcf7d2b909f1b6315b989a7a2b48fe38dbf9a
milleriishaun <milleriishaun@gmail.com>
2018-10-29 10:10:32 -0700

    Edit IntAlg_missing_lets

--- IntAlg_missing_letters.js
+++ IntAlg_missing_letters.js
@@ -1,21 +1,25 @@
-// my inital solution
+// my initial solution
 // time: 10 mins. //fail: didn't really work, so I had to move it here.
 function fearNotLetter(str) {
-  let arr = str.split();
-  // let start = 97;
-  // let end = 122;
-  let startx = arr[0];
-  let endx = arr[arr.length];
-  let code = 0;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = startx; j < endx; j++) {
-      if (arr[j] !== String.fromCharCode(j)) {
-        return (code = String.fromCharCode(j));
-      }
+  let count = 0;
+  let alpha = 'abcdefghijklmnopqrstuvwxyz';
+  let start = str[0];
+  let startx = 0;
+  for (let i = 0; i < alpha.length; i++) {
+    if (start === alpha[i]) {
+      startx = i;
     }
-    return undefined;
   }
-  return code;
+  for (let k = 0; k < str.length + 1; k++) {
+    if (str[k] !== alpha[k + startx]) {
+      return alpha[k + startx]; //only use startx, no need for endx
+    }
+  }
+  return undefined;
 }
 
-fearNotLetter('abce');
+console.log(fearNotLetter('abce')); // d
+console.log(fearNotLetter('abcdefghjklmno')); // i
+console.log(fearNotLetter('stvwx')); // u
+console.log(fearNotLetter('bcdf')); // e
+console.log(fearNotLetter('abcdefghijklmnopqrstuvwxyz')); // undefined
