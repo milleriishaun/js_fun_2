0d61d93a00b109df2e4dfff3c715a7f308c98f0c
milleriishaun <milleriishaun@gmail.com>
2018-11-13 09:52:04 -0800

    Add IntAlg_args_options.js

--- IntAlg_truthy_collection.js
+++ IntAlg_truthy_collection.js
@@ -1,10 +1,53 @@
+/*
 // my initial solution
-// time:  pm
-// debug time:  pm
+// time: 5 mins
+// debug time: 0 mins
+function truthCheck(collection, pre) {
+  return collection.every(
+    item => item.hasOwnProperty(pre) && Boolean(item[pre])
+  );
+}
+*/
+
+/*
+// Basic Solution
+function truthCheck(collection, pre) {
+  // Create a counter to check how many are true.
+  var counter = 0;
+  // Check for each object
+  for (var c in collection) {
+    // If it is has property and value is truthy
+    if (collection[c].hasOwnProperty(pre) && Boolean(collection[c][pre])) {
+      counter++;
+    }
+  }
+  // Outside the loop, check to see if we got true for all of them and return true or false
+  return counter == collection.length;
+}
+*/
+
+/*
+// Intermediate Solution
+function truthCheck(collection, pre) {
+  return collection.every(function(element) {
+    return element.hasOwnProperty(pre) && Boolean(element[pre]);
+  });
+}
+*/
+
+/*
+// Advanced Solution
 function truthCheck(collection, pre) {
   // Is everyone being true?
-  return pre;
+  return collection.every(obj => obj[pre]);
 }
+*/
+
+// Practice Session 2, 11//2018,  pm
+
+// time:  mins
+// debug time:  mins
+
 console.log(collection);
 console.log(
   truthCheck(
