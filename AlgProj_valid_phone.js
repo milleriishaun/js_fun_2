4d107ce55632d32d0d2080dfcf97204b716bef00
milleriishaun <milleriishaun@gmail.com>
2018-11-15 15:16:16 -0800

    Add AlgProj_cash_register.js

--- AlgProj_valid_phone.js
+++ AlgProj_valid_phone.js
@@ -14,7 +14,7 @@ function telephoneCheck(str) {
 */
 
 /*
-// Basic Solution
+// Basic Solution // this passes FCC tests
 function telephoneCheck(str) {
   var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
   return regex.test(str);
@@ -38,7 +38,7 @@ function telephoneCheck(str) {
 */
 
 /*
-// Intermediate Solution
+// Intermediate Solution //fail: this fails FCC tests, but good IRL
 function telephoneCheck(str) {
   var re = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})$/;
   return re.test(str);
