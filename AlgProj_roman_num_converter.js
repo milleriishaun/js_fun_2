66b5e2720de9af6a88224694ed5eb88e9a666488
milleriishaun <milleriishaun@gmail.com>
2018-11-15 13:40:17 -0800

    Edit AlgProj_roman_num_converter.js

--- AlgProj_roman_num_converter.js
+++ AlgProj_roman_num_converter.js
@@ -1,6 +1,7 @@
-// my initial solution
-// time:  mins
-// debug time:  mins
+/*
+// my initial solution(fail, don't try this at home!)
+// time: 60 mins
+// debug time: 240 mins //fail: longest problem I wanted to solve.
 function convertToRoman(num) {
   let arr = [];
   while (num > 0) {
@@ -44,34 +45,42 @@ function convertToRoman(num) {
   for (let j = 0; j < arr.length - 3; j++) {
     switch (arr.slice(j, j + 4).join('')) {
       case 'CCCC':
-        // console.log(arr, 'CCCC hit');
-        arr[j - 1] === 'D'
-          ? arr.splice(j - 1, 5, 'C', 'M') //, arr.splice((j, 0, 'V')))
-          : arr.splice(j + 1, 3, 'D');
-        // console.log(arr, 'CCCC hit');
-        j -= 4;
+        if (arr[j + 3] !== 'C') {
+          arr[j - 1] === 'D'
+            ? arr.splice(j - 1, 5, 'C', 'M')
+            : arr.splice(j + 1, 3, 'D');
+        } else {
+          if (arr[j + 4] !== 'L') {
+            arr.splice(j - 1, 5, 'M');
+          } else {
+            if (arr[j - 1] !== 'D') {
+              arr.splice(j - 10, 11, 'D');
+            } else {
+              if (arr[j + 9] !== 'X') {
+                arr.splice(j - 1, 5, 'C', 'M');
+              } else {
+                arr.splice(j - 1, 11, 'M');
+              }
+            }
+          }
+        }
+        j = -1;
         break;
       case 'XXXX':
-        // console.log(arr, 'XXXX hit');
         if (arr[j + 4] !== 'X') {
           arr[j - 1] === 'L'
-            ? arr.splice(j - 1, 5, 'X', 'C') //, arr.splice((j, 0, 'V')))
+            ? arr.splice(j - 1, 5, 'X', 'C')
             : arr.splice(j + 1, 3, 'L');
-          // console.log(arr, 'XXXX hit');
         } else {
-          // console.log(arr, 'XXXXX hit');
           arr.splice(j - 3, 8, 'D');
-          // console.log(arr, 'XXXXX hit');
         }
-        j -= 4;
+        j = -1;
         break;
       case 'IIII':
-        // console.log(arr, 'IIII hit');
         arr[j - 1] === 'V'
-          ? arr.splice(j - 1, 5, 'I', 'X') //, arr.splice((j, 0, 'V')))
+          ? arr.splice(j - 1, 5, 'I', 'X')
           : arr.splice(j + 1, 3, 'V');
-        // console.log(arr, 'IIII hit');
-        j -= 4;
+        j = -1;
         break;
       default:
         break;
@@ -79,6 +88,139 @@ function convertToRoman(num) {
   }
   return arr.join('');
 } //note: case object, not contents of the object array(so no case ['D', 'D'])
+*/
+
+/*
+// Basic Solution
+var convertToRoman = function(num) {
+  var decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var romanNumeral = [
+    'M',
+    'CM',
+    'D',
+    'CD',
+    'C',
+    'XC',
+    'L',
+    'XL',
+    'X',
+    'IX',
+    'V',
+    'IV',
+    'I'
+  ];
+
+  var romanized = '';
+
+  for (var index = 0; index < decimalValue.length; index++) {
+    while (decimalValue[index] <= num) {
+      romanized += romanNumeral[index];
+      num -= decimalValue[index];
+    }
+  }
+
+  return romanized;
+}; //note: someone would have to know this solution before they were tested.
+*/
+
+/*
+// Intermediate Solution 1
+function convertToRoman(num) {
+  var romans = ['I', 'V', 'X', 'L', 'C', 'D', 'M'],
+    ints = [],
+    romanNumber = [],
+    numeral = '';
+  while (num) {
+    ints.push(num % 10);
+    num = Math.floor(num / 10);
+  }
+  for (i = 0; i < ints.length; i++) {
+    units(ints[i]);
+  }
+  function units() {
+    numeral = romans[i * 2];
+    switch (ints[i]) {
+      case 1:
+        romanNumber.push(numeral);
+        break;
+      case 2:
+        romanNumber.push(numeral.concat(numeral));
+        break;
+      case 3:
+        romanNumber.push(numeral.concat(numeral).concat(numeral));
+        break;
+      case 4:
+        romanNumber.push(numeral.concat(romans[i * 2 + 1]));
+        break;
+      case 5:
+        romanNumber.push(romans[i * 2 + 1]);
+        break;
+      case 6:
+        romanNumber.push(romans[i * 2 + 1].concat(numeral));
+        break;
+      case 7:
+        romanNumber.push(romans[i * 2 + 1].concat(numeral).concat(numeral));
+        break;
+      case 8:
+        romanNumber.push(
+          romans[i * 2 + 1]
+            .concat(numeral)
+            .concat(numeral)
+            .concat(numeral)
+        );
+        break;
+      case 9:
+        romanNumber.push(romans[i * 2].concat(romans[i * 2 + 2]));
+    }
+  }
+  return romanNumber
+    .reverse()
+    .join('')
+    .toString();
+} //note: I would never get this without studying or something.
+*/
+
+/*
+// Intermediate Solution 2
+function convertToRoman(num) {
+  var romans = [
+      // 10^i 10^i*5
+      ['I', 'V'], // 10^0
+      ['X', 'L'], // 10^1
+      ['C', 'D'], // 10^2
+      ['M'] // 10^3
+    ],
+    digits = num
+      .toString()
+      .split('')
+      .reverse()
+      .map(function(item, index) {
+        return parseInt(item);
+      }),
+    numeral = '';
+
+  // Loop through each digit, starting with the ones place
+  for (var i = 0; i < digits.length; i++) {
+    // Make a Roman numeral that ignores 5-multiples and shortening rules
+    numeral = romans[i][0].repeat(digits[i]) + numeral;
+    // Check for a Roman numeral 5-multiple version
+    if (romans[i][1]) {
+      numeral = numeral
+        // Change occurrences of 5 * 10^i to the corresponding 5-multiple Roman numeral
+        .replace(romans[i][0].repeat(5), romans[i][1])
+        // Shorten occurrences of 9 * 10^i
+        .replace(
+          romans[i][1] + romans[i][0].repeat(4),
+          romans[i][0] + romans[i + 1][0]
+        )
+        // Shorten occurrences of 4 * 10^i
+        .replace(romans[i][0].repeat(4), romans[i][0] + romans[i][1]);
+    }
+  }
+
+  return numeral;
+} // note: this is not easy either, but the steps are clear which is nice.
+*/
 
 console.log(convertToRoman(2)); //"II"
 console.log(convertToRoman(3)); //"III"
