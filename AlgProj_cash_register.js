93516db36536ceed60eff2401dea09790bd51531
milleriishaun <milleriishaun@gmail.com>
2018-12-17 10:55:51 -0800

    Edit AlgProj C register

--- AlgProj_cash_register.js
+++ AlgProj_cash_register.js
@@ -211,6 +211,20 @@ console.log(
   ])
 ); //{status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}
 
+console.log(
+  checkCashRegister(19.5, 20, [
+    ['PENNY', 0.01],
+    ['NICKEL', 0],
+    ['DIME', 0],
+    ['QUARTER', 0],
+    ['ONE', 1],
+    ['FIVE', 0],
+    ['TEN', 0],
+    ['TWENTY', 0],
+    ['ONE HUNDRED', 0]
+  ])
+); //{status: "INSUFFICIENT_FUNDS", change: []}
+
 // Beginner Solution
 // Create an array of objects which hold the denominations and their values
 var denom = [
