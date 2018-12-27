bd952e511179f4ff20154ae3ffcae96abfdba313
milleriishaun <milleriishaun@gmail.com>
2018-12-27 15:11:08 -0800

    Edit Pali test 11

--- AlgProj_palindrome_checker.js
+++ AlgProj_palindrome_checker.js
@@ -113,5 +113,6 @@ console.log(palindrome('never odd or even')); // true
 console.log(palindrome('My age is 0, 0 si ega ym.')); // true
 console.log(palindrome('0_0 (: /- :) 0-0')); // true
 console.log(palindrome('not a palindrome')); // false
+console.log(palindrome('nonpalindrome')); // false
 console.log(palindrome('1 eye for of 1 eye.')); // false
 console.log(palindrome('five|_/|four')); // false
