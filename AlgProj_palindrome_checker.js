671be43698466e164638100e15b6570715f4084a
milleriishaun <milleriishaun@gmail.com>
2019-01-04 12:14:42 -0800

    Edit comment pali

--- AlgProj_palindrome_checker.js
+++ AlgProj_palindrome_checker.js
@@ -112,7 +112,7 @@ console.log(palindrome('A Toyota! Race fast, safe car! A Toyota!')); // true
 console.log(palindrome('A nut for a jar of tuna')); // true
 console.log(palindrome('A man, a plan, a canal. Panama')); // true
 console.log(palindrome('never odd or even')); // true
-console.log(palindrome('0_0 (: /- :) 0-0')); // true
+console.log(palindrome('0_0 (: /- :) 0-0')); // true ... palindrome busted
 console.log(palindrome('not a palindrome')); // false
 console.log(
   palindrome('not a heroic medieval knight with a remote control helicopter')
