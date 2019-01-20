51944d3a24b884ce03f8cb689e499202b05cc491
milleriishaun <milleriishaun@gmail.com>
2019-01-20 07:41:20 -0800

    Add type coercion basic_JS.js

--- basic_JS.js
+++ basic_JS.js
@@ -54,3 +54,21 @@ function fn() {
 console.log(fn());
 console.log(typeof fn());
 console.log(fn(3, 3, 3, 3));
+console.log('bro');
+console.log(true + false); // 1
+console.log(12 / '6'); // 2
+console.log('number' + 15 + 3); // 'number153'
+console.log(15 + 3 + 'number'); // '18number'
+console.log([1] > null); // true
+console.log('foo' + +'bar'); // 'fooNaN'
+console.log('true' == true); // false
+console.log(false == 'false'); // false
+console.log(null == ''); // false
+console.log(!!'false' == !!'true'); // true
+console.log(['x'] == 'x'); // true
+console.log([] + null + 1); // 'null1'
+console.log([1, 2, 3] == [1, 2, 3]); // false
+console.log({} + [] + {} + [1]); // '0[object Object]1'
+console.log(!+[] + [] + ![]); // 'truefalse'
+console.log(new Date(0) - 0); // 0
+console.log(new Date(0) + 0); // 'Thu Jan 01 1970 02:00:00(EET)0'
